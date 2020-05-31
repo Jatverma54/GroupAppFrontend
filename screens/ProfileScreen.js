fbfe7644ff7d61a090d3d6ed92fb85eea85801a4
Verma <jatverma@deloitte.com>
2020-06-01 00:35:35 +0530

    added camera options

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -3,23 +3,124 @@ import {
   StyleSheet,
   Text,
   View,
+  TouchableOpacity,
   Image,
 } from 'react-native';
 
+
+import Email_Icon from '../Pictures/Email.png';
+import lock_Icon from '../Pictures/lock.png';
+import person from '../Pictures/person.png';
+import DrawerLogo from '../Pictures/DrawerLogo.png';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import FAIcon from "react-native-vector-icons/FontAwesome";
+import MDIcon from "react-native-vector-icons/MaterialIcons";
+import RBSheet from "react-native-raw-bottom-sheet";
+
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+
+
+FAIcon.loadFont();
+MDIcon.loadFont();
+
+
 export default class ProfileScreen extends Component {
+
+
+  constructor(props) {
+    super(props);
+  }
+
+  state = {    
+       photo: null,
+       
+    };
+
+    componentDidMount() {
+      this.getPermissionAsync();
+      this.getCameraPermissionAsync();
+    }
+  
+  
+    getPermissionAsync = async () => {
+      if (Constants.platform.ios) {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    };
+  
+  
+     _pickImage = async () => {
+      try {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+        if (!result.cancelled) {
+          this.setState({ photo: result.uri });
+          this.CameraOptions.close(); 
+        }
+  
+        console.log(result);
+      } catch (E) {
+        console.log(E);
+      }
+   
+  };
+  
+  getCameraPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  };
+  
+  
+   _clickImage = async () => {
+    try {
+      let result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        this.setState({ photo: result.uri });
+        this.CameraOptions.close(); 
+      }
+  
+      console.log(result);
+    } catch (E) {
+      console.log(E);
+    }
+  
+  };
+    
+
+
 
   render() {
     return (
       <View style={styles.container}>
           <View style={styles.header}>
+          <TouchableOpacity  onPress={() => this.CameraOptions.open()}>
             <View style={styles.headerContent}>
                 <Image style={styles.avatar}
-                  source={{uri: 'https://bootdey.com/img/Content/avatar/avatar1.png'}}/>
+                  source={{uri: this.state.photo}}/>
 
                 <Text style={styles.name}>
                   Jatin Verma
                 </Text>
             </View>
+            </TouchableOpacity>
           </View>
 
           <View style={styles.body}>
@@ -35,6 +136,52 @@ export default class ProfileScreen extends Component {
              
             </View>
         </View>
+
+
+
+
+{/* List Menu */}
+<RBSheet
+          ref={ref => {
+            this.CameraOptions = ref;
+          }}
+          height={330}
+        >
+          <View style={styles.listContainer}>
+            <Text style={styles.listTitle}>Create</Text>
+          
+              <TouchableOpacity
+                
+                style={styles.listButton}
+                onPress={() => this._clickImage()}
+              >
+                <MDIcon name="photo-camera" style={styles.listIcon} />
+                <Text style={styles.listLabel}>Take photo</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                
+                style={styles.listButton}
+                onPress={() => this._pickImage()}
+              >
+                <MDIcon name="photo" style={styles.listIcon} />
+                <Text style={styles.listLabel}>Choose image</Text>
+              </TouchableOpacity>
+           
+          </View>
+        </RBSheet>
+
+
+
+
+
+
+
+
+
+
+
+
       </View>
     );
   }
@@ -70,6 +217,29 @@ const styles = StyleSheet.create({
     fontSize:18,
     marginTop:20,
     color: "white",
-  }
+  },
+  listContainer: {
+    flex: 1,
+    padding: 25,
+    
+  },
+  listTitle: {
+    fontSize: 16,
+    marginBottom: 20,
+    color: "#666"
+  },
+  listButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 10
+  },
+  listIcon: {
+    fontSize: 26,
+    color: "#666",
+    width: 60
+  },
+  listLabel: {
+    fontSize: 16
+  },
 });
  
