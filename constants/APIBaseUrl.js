3e53d6bd490d976ac656d0a84ee0a753a34620d8
Jatverma54 <jatinv94@gmail.com>
2021-02-18 01:22:12 +0530

    Modified Joined Group Section

--- constants/APIBaseUrl.js
+++ constants/APIBaseUrl.js
@@ -1,5 +1,5 @@
 export default{
 
-   BaseUrl:'http://192.168.0.105:3000'//'http://GroupAppbackend-dev.ap-south-1.elasticbeanstalk.com'
+   BaseUrl:'http://GroupAppbackend-dev.ap-south-1.elasticbeanstalk.com'//'http://192.168.0.105:3000'
    
    };
