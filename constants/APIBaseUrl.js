960b5ebe0a5c7e6dd896e77462b3a75105c672f5
Jatverma54 <64661506+Jatverma54@users.noreply.github.com>
2025-01-14 17:37:17 +0530

    Update APIBaseUrl.js

--- constants/APIBaseUrl.js
+++ constants/APIBaseUrl.js
@@ -1,17 +1,10 @@
 export default{
 
  BaseUrl:'https://d3f8dq47pv4rsx.cloudfront.net',
-  //  CreateaPublicGroupScreenRewardedAd:'ca-app-pub-1558609691925120/2574551042',
-  //  CreateaPersonalGroupScreenRewardedAd:'ca-app-pub-1558609691925120/5939080989',
-  //  PublicGroupBioScreenINTAd:'ca-app-pub-1558609691925120/8156860849',
-  //  PersonalGroupBioScreenINTAd:'ca-app-pub-1558609691925120/7658167397',
-  //  JoinedGroupBioScreenINTAd:'ca-app-pub-1558609691925120/6245654001',
-  //  ProfileScreenINTAd:'ca-app-pub-1558609691925120/1289389530'
-//  BaseUrl:'http://192.168.1.69:3000',
-   CreateaPublicGroupScreenRewardedAd:'ca-app-pub-3940256099942544/5224354917',
-   CreateaPersonalGroupScreenRewardedAd:'ca-app-pub-3940256099942544/5224354917',
-   PublicGroupBioScreenINTAd:'ca-app-pub-3940256099942544/1033173712',
-   PersonalGroupBioScreenINTAd:'ca-app-pub-3940256099942544/1033173712',
-   JoinedGroupBioScreenINTAd:'ca-app-pub-3940256099942544/1033173712',
-   ProfileScreenINTAd:'ca-app-pub-3940256099942544/1033173712'
+   CreateaPublicGroupScreenRewardedAd:'',
+   CreateaPersonalGroupScreenRewardedAd:'',
+   PublicGroupBioScreenINTAd:'',
+   PersonalGroupBioScreenINTAd:'',
+   JoinedGroupBioScreenINTAd:'',
+   ProfileScreenINTAd:''
    };
